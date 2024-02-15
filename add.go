db9e25d87d7b031c5faa61746ceb7c17362a5781
Chandum2plus <chandukumarbca@gmail.com>
2024-02-15 11:49:50 +0530

    normal code

--- add.go
+++ add.go
@@ -1,14 +1,18 @@
 package main
 
-type Math struct {
-	Sum  float64 `json:"sum"`
-	Num1 float64 `json:"num1"`
-	Num2 float64 `json:"num2"`
-}
+import "fmt"
 
-func handler() {
-
-}
+//	type Math struct {
+//		Sum  float64 `json:"sum"`
+//		Num1 float64 `json:"num1"`
+//		Num2 float64 `json:"num2"`
+//	}
+//
+// func handler() {
+//
+// }
 func main() {
-
+	fmt.Println("Hello World")
+	fmt.Print("Hello Github !")
+	
 }
